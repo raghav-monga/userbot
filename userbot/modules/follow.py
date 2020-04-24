b3bfb2d90bae78d88162b55856ac556e76a17695
Abhinav Shinde <64246779+abhinavshinde@users.noreply.github.com>
2020-04-24 18:11:12 +0530

    Update follow.py

--- userbot/modules/follow.py
+++ userbot/modules/follow.py
@@ -35,7 +35,7 @@ async def follow(follow):
     await follow.edit(
                      f"`FOLLOW {DEFAULTUSER} ON` \n\n"
                      f"[InstaGram](https://www.instagram.com/mayur_karaniya) \n\n"
-                     f"[FaceBook](https://www.facebook.com/mkaraniya) \n\n"
+                     f"[𝙁𝙖𝙘𝙚𝙗𝙤𝙤𝙠](https://www.facebook.com/mkaraniya) \n\n"
                      f"[YouTube](https://www.youtube.com/channel/UCeKQxQK7XZ3jGi3541uWATg?sub_confirmation=1) "
                      )    
 
