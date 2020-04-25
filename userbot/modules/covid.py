c9b7d8af718fd1109e530eb039ecfbf5ab32b34a
Abhinav Shinde <64246779+abhinavshinde@users.noreply.github.com>
2020-04-25 17:43:57 +0530

    Update covid.py

--- userbot/modules/covid.py
+++ userbot/modules/covid.py
@@ -17,10 +17,10 @@ async def corona(event):
     covid = Covid()
     country_data = covid.get_status_by_country_name(country)
     if country_data:
-        output_text =  f"`Confirmed   : {country_data['confirmed']}`\n"
-        output_text += f"`Active      : {country_data['active']}`\n"
-        output_text += f"`Deaths      : {country_data['deaths']}`\n"
-        output_text += f"`Recovered   : {country_data['recovered']}`\n"
+        output_text =  f"`🦠Confirmed   : {country_data['confirmed']}`\n"
+        output_text += f"`☢️Active      : {country_data['active']}`\n"
+        output_text += f"`⚰️Deaths      : {country_data['deaths']}`\n"
+        output_text += f"`♥️Recovered   : {country_data['recovered']}`\n"
         output_text += (
             "`Last update : "
             f"{datetime.utcfromtimestamp(country_data['last_update'] // 1000).strftime('%Y-%m-%d %H:%M:%S')}`\n"
