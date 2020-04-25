d0913b720f4c414aaac8154dc5f9a222ffe1fc63
Abhinav Shinde <64246779+abhinavshinde@users.noreply.github.com>
2020-04-25 18:06:27 +0530

    cmd changed .data to info

--- userbot/modules/whois.py
+++ userbot/modules/whois.py
@@ -17,7 +17,7 @@ from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY
 from userbot.events import register
 
 
-@register(pattern=".data(?: |$)(.*)", outgoing=True)
+@register(pattern=".info(?: |$)(.*)", outgoing=True)
 async def who(event):
 
     await event.edit(
@@ -149,6 +149,6 @@ async def fetch_info(replied_user, event):
 
 CMD_HELP.update({
     "User Info":
-    ".data <username> or reply to someones text with .data\
+    ".info <username> or reply to someones text with .info\
     \nUsage: Gets info of an user."
 })
