815e5297b9c17c55eff7d43c7e886f7b8fac1b3a
Abhinav Shinde <64246779+abhinavshinde@users.noreply.github.com>
2020-04-29 19:23:28 +0530

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -1,53 +1,51 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-""" Userbot module for managing events.
- One of the main components of the userbot. """
 
-import sys
-from asyncio import create_subprocess_shell as asyncsubshell
-from asyncio import subprocess as asyncsub
-from os import remove
-from time import gmtime, strftime
-from traceback import format_exc
-
-from telethon import events
-
-from userbot import bot, BOTLOG_CHATID, LOGSPAMMER
-
-
-def register(**args):
-    """ Register a new event. """
-    pattern = args.get('pattern', None)
-    disable_edited = args.get('disable_edited', False)
-    ignore_unsafe = args.get('ignore_unsafe', False)
-    unsafe_pattern = r'^[^/!#@\$A-Za-z]'
-    groups_only = args.get('groups_only', False)
-    trigger_on_fwd = args.get('trigger_on_fwd', False)
-    trigger_on_inline = args.get('trigger_on_inline', False)
-    disable_errors = args.get('disable_errors', False)
-
-    if pattern is not None and not pattern.startswith('(?i)'):
-        args['pattern'] = '(?i)' + pattern
-
-    if "disable_edited" in args:
-        del args['disable_edited']
-
-    if "ignore_unsafe" in args:
-        del args['ignore_unsafe']
-
-    if "groups_only" in args:
-        del args['groups_only']
-
-    if "disable_errors" in args:
-        del args['disable_errors']
-
-    if "trigger_on_fwd" in args:
-        del args['trigger_on_fwd']
       
-    if "trigger_on_inline" in args:
+in args:
+        del args['trigger_on_inline']
+
+    if pattern:
+        if not ignore_unsafe:
+            args['pattern'] = pattern.replace('^.', unsafe_pattern, 1)
+
+    def decorator(func):
+        async def wrapper(check):
+            if not LOGSPAMMER:
+                send_to = check.chat_id
+            else:
+                send_to = BOTLOG_CHATID
+
+            if not trigger_on_fwd and check.fwd_from:
+                return
+
+            if check.via_bot_id and not trigger_on_inline:
+                return
+             
+            if groups_only and not check.is_group:
+is a group.`")
+
+
+            try:
+                await func(check)
+
+            # Thanks to @kandnub for this HACK.
+            # Raise StopPropagation to Raise StopPropagation
+            # This needed for AFK to working properly
+
+            except events.StopPropagation:
+                raise events.StopPropagation
+            # This is a gay exception and must be passed out. So that it doesnt spam chats
+            except KeyboardInterrupt:
+                pass
+            except BaseException:
+
+                # Check if we have to disable it.
+                # If not silence the log spam on the console,
+                # with a dumb except.
+
+                if not disable_errors:
+                    date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+
+                    text = "**USERBOT ERROR REPOtrigger_on_inline" in args:
         del args['trigger_on_inline']
 
     if pattern:
@@ -93,6 +91,8 @@ def register(**args):
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
                     text = "**USERBOT ERROR REPORT**\n"
+                    link = "[OUB Support Chat](https://t.me/PPE_Support)"
+                    text += "If you want to, you can report RT**\n"
                     link = "[OUB Support Chat](https://t.me/PPE_Support)"
                     text += "If you want to, you can report it"
                     text += f"- just forward this message to {link}.\n"
