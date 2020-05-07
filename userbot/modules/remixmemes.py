dcca663898e107b33f8a08ed058076b315f69504
Abhinav Shinde <64624037+abhinavshinde@users.noreply.github.com>
2020-05-07 10:23:34 +0530

    Update remixmemes.py

--- userbot/modules/remixmemes.py
+++ userbot/modules/remixmemes.py
@@ -1124,8 +1124,8 @@ async def _(event):
             "**Chat Message Exported To** `./Inpu/`",
             "**Chat Message Exported To** `./Inpu/homework/`",
             "**Chat Message Exported To** `./Inpu/homework/groupchat.txt`",
-            "__Legend is leaving this chat.....! Bye geys..__",
-            "__Legend is leaving this chat.....! Bye geys..__"
+            "__Legend is leaving this chat.....! Bye guys..__",
+            "__Legend is leaving this chat.....! Bye guys..__"
 
  ]
 
